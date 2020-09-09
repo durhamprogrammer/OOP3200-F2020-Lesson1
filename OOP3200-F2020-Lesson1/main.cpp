a5288787018733ce773843be91609e08e42876ef
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-09 11:37:19 -0400

    added welcomeFunction

--- OOP3200-F2020-Lesson1/main.cpp
+++ OOP3200-F2020-Lesson1/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+void welcomeFunction()
+{
+	std::cout << "Welcome to the Course" << std::endl;
+}
+
+
+
 int main()
 {
+	
+	// This is my Hello World Message
     std::cout << "Hello World!\n";
+
+	welcomeFunction();
 }
 
 
